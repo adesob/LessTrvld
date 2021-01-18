721cd00fd11fe503671db50595490c366a7225d0
adesob <55300413+adesob@users.noreply.github.com>
2021-01-18 14:26:51 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+//import './App.css';
+import Pathfind from "./components/Pathfind"
+
+class App extends Component {
+  render(){
+    return (
+      <div>
+        <Pathfind />
+      </div>
+    );
+  }
+}
+
+export default App;
